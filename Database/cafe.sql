bbb3b4680f06fd72f3d8df54275eda26941f4be4
Rizki Yanuar <ryanuar83@gmail.com>
2023-12-08 01:25:04 +0700

    Penambahan Tabel Menu dan penambahan atribut foreign key pada tabel pemesanan di Database

--- Database/cafe.sql
+++ Database/cafe.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Des 2023 pada 15.05
--- Versi server: 10.4.27-MariaDB
--- Versi PHP: 8.0.25
+-- Generation Time: Dec 07, 2023 at 07:22 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `karyawan`
+-- Table structure for table `karyawan`
 --
 
 CREATE TABLE `karyawan` (
@@ -37,7 +37,21 @@ CREATE TABLE `karyawan` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pelanggan`
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `ID_Menu` int(11) NOT NULL,
+  `Nama_Menu` varchar(30) NOT NULL,
+  `Deskripsi` varchar(20) NOT NULL,
+  `Harga` int(10) NOT NULL,
+  `Kategori` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
 --
 
 CREATE TABLE `pelanggan` (
@@ -50,13 +64,14 @@ CREATE TABLE `pelanggan` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pemesan`
+-- Table structure for table `pemesan`
 --
 
 CREATE TABLE `pemesan` (
-  `ID_Pemesan` int(5) NOT NULL,
+  `ID_Pesanan` int(5) NOT NULL,
   `ID_Menu` int(5) NOT NULL,
   `ID_Pelanggan` int(5) NOT NULL,
+  `ID_Karyawan` int(5) NOT NULL,
   `Tanggal_Pemesanan` date NOT NULL,
   `Total_Harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -66,22 +81,43 @@ CREATE TABLE `pemesan` (
 --
 
 --
--- Indeks untuk tabel `karyawan`
+-- Indexes for table `karyawan`
 --
 ALTER TABLE `karyawan`
   ADD PRIMARY KEY (`ID_Karyawan`);
 
 --
--- Indeks untuk tabel `pelanggan`
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`ID_Menu`);
+
+--
+-- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`ID_Pelanggan`);
 
 --
--- Indeks untuk tabel `pemesan`
+-- Indexes for table `pemesan`
 --
 ALTER TABLE `pemesan`
-  ADD PRIMARY KEY (`ID_Pemesan`);
+  ADD PRIMARY KEY (`ID_Pesanan`),
+  ADD KEY `FK_Menu` (`ID_Menu`),
+  ADD KEY `FK_Pelanggan` (`ID_Pelanggan`),
+  ADD KEY `FK_Karyawan` (`ID_Karyawan`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pemesan`
+--
+ALTER TABLE `pemesan`
+  ADD CONSTRAINT `FK_Karyawan` FOREIGN KEY (`ID_Karyawan`) REFERENCES `karyawan` (`ID_Karyawan`),
+  ADD CONSTRAINT `FK_Menu` FOREIGN KEY (`ID_Menu`) REFERENCES `menu` (`ID_Menu`),
+  ADD CONSTRAINT `FK_Pelanggan` FOREIGN KEY (`ID_Pelanggan`) REFERENCES `pelanggan` (`ID_Pelanggan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

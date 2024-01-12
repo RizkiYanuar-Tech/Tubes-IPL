d48ea36d394ccd9b1a6a1a7fd350d12dcdf029cd
Rizki Yanuar <ryanuar83@gmail.com>
2024-01-12 08:17:30 +0700

    Pengerjaan dilakukan oleh rafly

---
+++ Implementasi UI&UX/index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <!-- Feather Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!--Ny Style-->
+    <link rel="stylesheet" href="css/style.css" />
+    <title>TUBES IPL</title>
+  </head>
+
+  <body>
+    <!-- Navbar Start-->
+    <nav class="navbar">
+      <a class="navbar-logo">Kopi <span>Saya.</span></a>
+
+      <div class="navbar-nav">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="menu.php">Menu</a>
+        <a href="#contact">Kontak</a>
+      </div>
+
+      <div class="navbar-extra">
+        <a href="#" id="hamburger-menu"><i data-feather="menu"></i> </a>
+      </div>
+    </nav>
+
+    <!-- Navbar End-->
+
+    <!-- Hero Section Start-->
+    <section class="hero" id="home">
+      <main class="content">
+        <h1>Mari Nikmati <span>Kopi dan Makanan</span></h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+          doloribus. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Quidem minima corporis nobis repellat dolorum explicabo.
+        </p>
+      </main>
+    </section>
+
+    <!-- Hero Section End-->
+
+    <!-- About Section Start-->
+    <section id="about" class="about">
+      <div class="about1">
+        <div class="a">
+          <img src="img/1.png" alt="1" />
+          <h2><span>AWESOME</span> AROMA</h2>
+          <p>The coffe is brewed by first roasting the green coffe beans</p>
+        </div>
+        <div class="a">
+          <img src="img/2.png" alt="1" />
+          <h2><span>HIGH</span> QUALITY</h2>
+          <p>The coffe is brewed by first roasting the green coffe beans</p>
+        </div>
+        <div class="a">
+          <img src="img/3.png" alt="1" />
+          <h2><span>PURE</span> GRADES</h2>
+          <p>The coffe is brewed by first roasting the green coffe beans</p>
+        </div>
+        <div class="a">
+          <img src="img/4.png" alt="1" />
+          <h2><span>PROPER</span> ROASTING</h2>
+          <p>The coffe is brewed by first roasting the green coffe beans</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="about-img">
+          <img src="img/about.jpg" alt="About" />
+        </div>
+
+        <div class="content">
+          <h1>Best Coffe House In Your Home Town</h1>
+          <p>
+            Our incridibly rate come from humble beginnings in yemen, where
+            decades of political turmail once forced local farmers tostart
+            growing khat, a narcotic native to the Arabian Peninsula.
+          </p>
+          <p>
+            The Dawoodi Bohara Community changed this, removing all the khat
+            plants and replacing them with coffe, briging this hu,ble brew black
+            to its roots.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <!-- About Section End-->
+
+    <!-- Contact Section Start-->
+
+    <section id="contact" class="contact">
+      <div class="contact1">
+        <div class="contact-img">
+          <img src="img/contact.png" alt="contact" class="center-img" />
+          <h1>Subscribe To <span>Get News</span></h1>
+        </div>
+      </div>
+      <div class="content"></div>
+    </section>
+
+    <!-- Contact Section End-->
+
+    <!-- Feather Icons -->
+    <script>
+      feather.replace();
+    </script>
+
+    <!-- My Javascript-->
+    <script src="js/script.js"></script>
+
+    <!-- Footer Start -->
+    <footer class="footer">
+      <div class="footer-left">
+        <h3></h3>
+        <div class="footer-logo">
+          <img src="img\Kopi Saya..png" alt="" />
+        </div>
+        <p class="footer-copyright">
+          The managing director & cluster head in Indonesia at Bandung
+        </p>
+      </div>
+
+      <div class="footer-center">
+        <div>
+          <i class=""></i>
+          <p>Contact US</p>
+        </div>
+        <div>
+          <i class="fa fa-map-marker"></i>
+          <p>Address CPI 21 at Cimahi, Indonesia</p>
+        </div>
+        <div>
+          <i class="fa fa-phone"></i>
+          <p>08512384955</p>
+        </div>
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p>coffe@gmail.com</p>
+        </div>
+      </div>
+
+      <div class="footer-right">
+        <div>
+          <i class=""></i>
+          <p>Viewers Guides</p>
+        </div>
+        <div>
+          <i class=""></i>
+          <p>Home</p>
+        </div>
+        <div>
+          <i class=""></i>
+          <p>About</p>
+        </div>
+        <div>
+          <i class=""></i>
+          <p>Coffe</p>
+        </div>
+      </div>
+    </footer>
+    <!-- Footer End -->
+  </body>
+</html>
